a645e31e03637f698008971e9b45d5cdc6af6eff
Tesmi <88522894+Tesmi-Develop@users.noreply.github.com>
2026-09-26 15:07:36 +1000

    ref: removed UserId

--- FefuScheduleBot/Schemas/UserSetting.cs
+++ FefuScheduleBot/Schemas/UserSetting.cs
@@ -5,7 +5,6 @@ namespace FefuScheduleBot.Schemas;
 [Serializable, Collection("UserSettings"), PublicAPI]
 public class UserSetting : Schema
 {
-    public long UserId;
     public List<string> Subgroups = [];
     
     public UserSetting(string id) : base(id)
